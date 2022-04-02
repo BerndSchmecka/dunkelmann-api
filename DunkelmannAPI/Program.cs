9db679e4cb66cbd4bd8fd0bf0798990af68060db
Dunkelmann <business@dunkelmann.eu>
2022-04-02 18:29:42 +0200

    Small improvements to Program.cs

--- DunkelmannAPI/Program.cs
+++ DunkelmannAPI/Program.cs
@@ -5,9 +5,9 @@ namespace DunkelmannAPI
     {
         public static string ERROR_TEMPLATE(string error_message) {return String.Format("<html><head><title>{0}</title></head><body><center><h1>{0}</h1></center><hr><center>{1}</center></body></html>", error_message, displayableVersion);}
 
-        public static Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        public static Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version ?? new Version(0, 0, 0);
         public static DateTime buildDate = new DateTime(2000, 1, 1).AddDays(version.Build).AddSeconds(version.Revision * 2);
-        public static string displayableVersion = null;
+        public static string? displayableVersion = null;
 
         public static DateTime epoch = new System.DateTime(1970,1,1,0,0,0,DateTimeKind.Utc);
 
